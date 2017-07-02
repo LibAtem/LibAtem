91926d06b9e029802dab3479e75b0fabc7498cbf
Julian <git@julusian.co.uk>
2017-07-02 18:58:44 +0100

    Better test output

--- LibAtem.Test/Commands/TestAutoSerialize.cs
+++ LibAtem.Test/Commands/TestAutoSerialize.cs
@@ -6,52 +6,61 @@ using System.Text;
 using LibAtem.Commands;
 using LibAtem.Test.Util;
 using Xunit;
+using Xunit.Abstractions;
 
 namespace LibAtem.Test.Commands
 {
     public class TestAutoSerialize
     {
+        private readonly ITestOutputHelper output;
 
-//        [Fact]
-//        public void TestCommandSerialization()
-//        {
-//            Assembly assembly = typeof(ICommand).GetTypeInfo().Assembly;
-//            IEnumerable<Type> types = assembly.GetTypes().Where(t => typeof(ICommand).GetTypeInfo().IsAssignableFrom(t))
-//                .Where(t => !SkipTypes.Contains(t));
-//
-//            var failures = new StringBuilder();
-//
-//            foreach (Type type in types)
-//            {
-//                if (type == typeof(ICommand))
-//                    continue;
-//                try
-//                {
-//                    ICommand cmd = (ICommand)Activator.CreateInstance(type);
-//                    Assert.NotNull(cmd);
-//
-//                    byte[] byteArray = cmd.ToByteArray();
-//                    if (byteArray.Length % 2 != 0)
-//                        throw new Exception("Expected command length to be an even number");
-//
-//                    var cmd2 = DeserializeSingle(byteArray);
-//
-//                    // TODO assert contents matches!
-//                }
-//                catch (Exception e)
-//                {
-//                    failures.AppendFormat("{0}: {1}\n", type.Name, e.Message);
-//                }
-//            }
-//
-//            string str = failures.ToString();
-//            Console.Error.WriteLine(str);
-//            Assert.Equal("", str);
-//        }
+        public TestAutoSerialize(ITestOutputHelper output)
+        {
+            this.output = output;
+        }
+
+        //        [Fact]
+        //        public void TestCommandSerialization()
+        //        {
+        //            Assembly assembly = typeof(ICommand).GetTypeInfo().Assembly;
+        //            IEnumerable<Type> types = assembly.GetTypes().Where(t => typeof(ICommand).GetTypeInfo().IsAssignableFrom(t))
+        //                .Where(t => !SkipTypes.Contains(t));
+        //
+        //            var failures = new StringBuilder();
+        //
+        //            foreach (Type type in types)
+        //            {
+        //                if (type == typeof(ICommand))
+        //                    continue;
+        //                try
+        //                {
+        //                    ICommand cmd = (ICommand)Activator.CreateInstance(type);
+        //                    Assert.NotNull(cmd);
+        //
+        //                    byte[] byteArray = cmd.ToByteArray();
+        //                    if (byteArray.Length % 2 != 0)
+        //                        throw new Exception("Expected command length to be an even number");
+        //
+        //                    var cmd2 = DeserializeSingle(byteArray);
+        //
+        //                    // TODO assert contents matches!
+        //                }
+        //                catch (Exception e)
+        //                {
+        //                    failures.AppendFormat("{0}: {1}\n", type.Name, e.Message);
+        //                }
+        //            }
+        //
+        //            string str = failures.ToString();
+        //            Console.Error.WriteLine(str);
+        //            Assert.Equal("", str);
+        //        }
 
         [Fact]
         public void TestAutoPropertySerialization()
         {
+            var failures = new List<string>();
+
             Assembly assembly = typeof(ICommand).GetTypeInfo().Assembly;
             IEnumerable<Type> types = assembly.GetTypes().Where(t => typeof(SerializableCommandBase).GetTypeInfo().IsAssignableFrom(t));
             foreach (Type type in types)
@@ -59,9 +68,19 @@ namespace LibAtem.Test.Commands
                 if (type == typeof(SerializableCommandBase))
                     continue;
 
-                Console.WriteLine("Testing: {0}", type.Name);
-                TestSingle(type, 10);
+                try
+                {
+                    output.WriteLine("Testing: {0}", type.Name);
+                    TestSingle(type, 10);
+                }
+                catch (Exception e)
+                {
+                    failures.Add(string.Format("{0}: {1}", type.Name, e.Message));
+                }
             }
+
+            output.WriteLine(string.Join("\n", failures));
+            Assert.Equal(0, failures.Count);
         }
 
         private static void TestSingle(Type t, int rounds)
@@ -70,8 +89,8 @@ namespace LibAtem.Test.Commands
             {
                 ICommand raw = RandomCommandGenerator.Create(t);
                 ICommand cmd = DeserializeSingle(raw.ToByteArray());
-                Assert.NotNull(cmd);
-                Assert.True(t.GetTypeInfo().IsAssignableFrom(cmd.GetType()), "Deserialized command of wrong type");
+                if (!t.GetTypeInfo().IsAssignableFrom(cmd.GetType()))
+                    throw new Exception("Deserialized command of wrong type");
 
                 RandomCommandGenerator.AssertAreTheSame(raw, cmd);
             }
@@ -91,12 +110,14 @@ namespace LibAtem.Test.Commands
             var parsed = new ParsedCommand(arr[2], arr[3], name, cmdBody);
 
             Type type = CommandManager.FindForName(name);
-            Assert.NotNull(type);
+            if (type == null)
+                throw new Exception("Failed to find command during deserialize");
 
             ICommand cmd = (ICommand)Activator.CreateInstance(type);
-            Assert.NotNull(cmd);
-            cmd.Deserialize(parsed);
+            if (cmd == null)
+                throw new Exception("Failed to construct command");
 
+            cmd.Deserialize(parsed);
             return cmd;
         }
     }
