e19f82d15ceac096661659758482778de94b5d7c
Julian Waller <git@julusian.co.uk>
2018-04-29 12:14:17 +0100

    Add some command validation that can be used before sending a command

---
+++ LibAtem.DeviceProfile/CommandValidator.cs
@@ -0,0 +1,28 @@
+﻿using LibAtem.Commands;
+using LibAtem.Serialization;
+
+namespace LibAtem.DeviceProfile
+{
+    public static class CommandValidator
+    {
+        public static bool IsValid(DeviceProfile profile, ICommand cmd)
+        {
+            if (!(cmd is AutoSerializeBase asbCmd))
+                return true;
+
+            var props = AutoSerializeBase.GetPropertySpecForType(asbCmd.GetType());
+
+            foreach (AutoSerializeBase.PropertySpec prop in props.Properties)
+            {
+                if (prop.Getter == null)
+                    continue;
+
+                object val = prop.Getter.DynamicInvoke(cmd);
+                if (!prop.SerAttr.IsValid(prop.PropInfo, val) || !AvailabilityChecker.IsAvailable(profile, val))
+                    return false;
+            }
+
+            return true;
+        }
+    }
+}
