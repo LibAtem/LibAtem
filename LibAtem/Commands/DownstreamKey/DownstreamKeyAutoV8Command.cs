fc7ddd59ef63e576a296f683b47464ad8982e11a
Julian Waller <git@julusian.co.uk>
2019-09-16 15:05:59 +0100

    feat: fix dsk auto commands

---
+++ LibAtem/Commands/DownstreamKey/DownstreamKeyAutoV8Command.cs
@@ -0,0 +1,33 @@
+using System.Collections.Generic;
+using LibAtem.Common;
+using LibAtem.MacroOperations;
+using LibAtem.MacroOperations.DownStreamKey;
+using LibAtem.Serialization;
+
+namespace LibAtem.Commands.DownstreamKey
+{
+    [CommandName("DDsA", ProtocolVersion.V8_0, 4)]
+    public class DownstreamKeyAutoV8Command : SerializableCommandBase
+    {
+        [Flags]
+        public enum MaskFlags
+        {
+            IsTowardsOnAir = 1 << 0,
+        }
+
+        [Serialize(0), Enum8]
+        public MaskFlags Mask { get; set; }
+        [CommandId]
+        [Serialize(1), Enum8]
+        public DownstreamKeyId Index { get; set; }
+        [Serialize(2), Bool]
+        public bool IsTowardsOnAir { get; set; }
+
+        public override IEnumerable<MacroOpBase> ToMacroOps(ProtocolVersion version)
+        {
+            // TODO - if has IsTowardsOnAir
+
+            yield return new DownstreamKeyAutoMacroOp { KeyIndex = Index };
+        }
+    }
+}
