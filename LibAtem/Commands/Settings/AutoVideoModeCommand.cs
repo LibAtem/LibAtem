22e839fa9e122eeb75e44aee4038976c88ef509c
Julian Waller <git@julusian.co.uk>
2020-08-10 22:42:10 +0100

    fix: typo

--- LibAtem/Commands/Settings/AutoVideoModeCommand.cs
+++ LibAtem/Commands/Settings/AutoVideoModeCommand.cs
@@ -2,7 +2,7 @@
 
 namespace LibAtem.Commands.Settings
 {
-    [CommandName("AiVM", CommandDirection.ToServer, 4), NoCommandId]
+    [CommandName("AiVM", CommandDirection.Both, 4), NoCommandId]
     public class AutoVideoModeCommand : SerializableCommandBase
     {
         [Serialize(0), Bool]
